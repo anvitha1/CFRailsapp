a50c5ab69e8d707a1d66b32d2c0c5d70bdf8c788
anvitha vallabhaneni <anvitha1@yahoo.com>
2017-09-08 16:55:39 +0000

    4.4 correction & 4.5 added carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,12 @@ class StaticPagesController < ApplicationController
   def index
   end
   
+  # def landing_page
+  #   @featured_product = Product.first
+  # end
+  
   def landing_page
-    @featured_product = Product.first
+    @products = Product.limit(3)
   end
   
 end
