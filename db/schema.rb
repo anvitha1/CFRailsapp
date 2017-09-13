a82da28201f09bb184361b1dc814aa10ff117469
anvitha vallabhaneni <anvitha1@yahoo.com>
2017-09-13 01:31:24 +0000

    4.6 revisions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908132019) do
+ActiveRecord::Schema.define(version: 20170913012134) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170908132019) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
-    t.float    "price"
+    t.decimal  "price"
   end
 
 end
