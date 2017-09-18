b596382eb654c7e4225da72d665567fc61e53d91
anvitha vallabhaneni <anvitha1@yahoo.com>
2017-09-18 17:13:27 +0000

    5.3 sending emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,10 +14,7 @@ class StaticPagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
-    ActionMailer::Base.mail(from: @email,
-        to: 'app75879458@heroku.com',
-        subject: "A new contact form message from #{@name}",
-        body: @message).deliver_now
+     UserMailer.contact_form(@email, @name, @message).deliver_now
   end
   
 end
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "anvitha1@gmail.com"
+  layout 'mailer'
+end
