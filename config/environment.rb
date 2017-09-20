ae71b3180dad2c9435cb9641b19695bd2933d512
anvitha vallabhaneni <anvitha1@yahoo.com>
2017-09-20 18:56:13 +0000

    5.3 corrections

--- config/environment.rb
+++ config/environment.rb
@@ -5,11 +5,11 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'sandbox7f9b5650cde44e9d863760b5d4c2120d.mailgun.org',
-  :authentication => :plain,
+  address: 'smtp.sendgrid.net',
+  port: '587',
+  authentication: :plain,
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'heroku.com',
+  enable_starttls_auto: true
 }
-ActionMailer::Base.delivery_method = :smtp
