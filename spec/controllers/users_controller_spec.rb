17f35b46d3a0442a55d90b9b98dd758af1936694
anvitha vallabhaneni <anvitha1@yahoo.com>
2017-10-31 01:07:05 +0000

    Functional tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe UsersController, type: :controller do
+describe UsersController, :type => :controller do
   
   before do
     @user = User.create!(email: "test@gmail.com", password: "12345678") 
@@ -8,22 +8,24 @@ describe UsersController, type: :controller do
   end
   
   describe 'GET #show' do
+    
     context 'when a user is logged in' do
       before do 
         sign_in @user
       end
       
       it 'loads correct user details' do
-        get :show, params: { id: @user.id }
-        expect(response).to be_ok
-        #expect(response).to have_http_status(200)
+        get :show, id: @user.id
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
         expect(assigns(:user)).to eq @user
       end
+      
     end
 
     context 'when a user is not logged in' do
       it 'redirects to login' do
-        get :show, params: { id: @user.id }
+        get :show, id: @user.id
         expect(response).to redirect_to(new_user_session_path)
       end
     end
@@ -34,7 +36,7 @@ describe UsersController, type: :controller do
       end
       
       it "redirect to root path" do
-        get :show, params: { id: @user2.id }
+        get :show, id: @user.id
         expect(response).to redirect_to(root_path)
       end
     end
