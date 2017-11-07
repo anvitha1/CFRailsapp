f6ea579a8ff885083ad2aad1b201907fe2699df2
anvitha vallabhaneni <anvitha1@yahoo.com>
2017-11-07 14:55:46 +0000

    Debugging

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,6 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
  def index
-  byebug
   if params[:q]
     search_term = params[:q]
     logger.debug "Product: #{@products}"
