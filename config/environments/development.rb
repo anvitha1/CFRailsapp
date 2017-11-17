72afa297ab3094ab2976cddae99a44ab6d4718a1
anvitha vallabhaneni <anvitha1@yahoo.com>
2017-11-17 15:27:00 +0000

    Caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,17 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
-    }
-  else
-    config.action_controller.perform_caching = false
-    config.cache_store = :null_store
-  end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -51,7 +41,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
- config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
- #any dynamic links that show up inside emails from your development environment generate the right URLs.
+#any dynamic links that show up inside emails from your development environment generate the right URLs.
 end
